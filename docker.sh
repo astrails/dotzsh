0857c6a8a700b2399ddaf8d407e2baace95a5d4d
Vitaly Kushner <vitaly@astrails.com>
2015-07-02 03:11:47 +0200

    docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,3 @@
-source ~/.digitaloceanrc
-
 alias ddr="docker run -it --rm"
 alias ds="docker swarm"
 alias dm="docker machine"
@@ -16,4 +14,27 @@ function dmu
   export DOCKER_IP=$(docker-machine ip "$@")
 }
 
-source ~/.docker/.machine.sh
+if [ -e ~/.docker/.machine.sh ]; then
+  source ~/.docker/.machine.sh
+fi
+
+if [ -e ~/.digitaloceanrc ]; then
+  source ~/.digitaloceanrc
+fi
+
+
+DOCKER="`which docker`"
+function docker()
+{
+  if [ -n "$1" ]; then
+    local cmd="$1"; shift
+
+    if which "docker-$cmd" > /dev/null; then
+      "docker-$cmd" "$@"
+    else
+      "$DOCKER" "$cmd" "$@"
+    fi
+  else
+    "$DOCKER"
+  fi
+}
